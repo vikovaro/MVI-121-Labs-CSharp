80d72c372fbc3d959163e15baac1b9e80812a104
vikovaro <98817333+vikovaro@users.noreply.github.com>
2023-04-07 16:49:17 +0300

    Add files via upload

---
+++ lab5/lab5/Form1.Designer.cs
@@ -0,0 +1,261 @@
+﻿namespace lab5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.radioButton4);
+            this.panel1.Controls.Add(this.radioButton3);
+            this.panel1.Controls.Add(this.radioButton2);
+            this.panel1.Controls.Add(this.radioButton1);
+            this.panel1.Location = new System.Drawing.Point(43, 26);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(329, 453);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(33, 133);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(199, 20);
+            this.radioButton4.TabIndex = 3;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "Объект 4 - Прямоугольник";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.CheckedChanged += new System.EventHandler(this.radioButton4_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(33, 107);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(144, 20);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Объект 3 - Сектор";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(33, 81);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(176, 20);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Объект 2 - Окружность";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(33, 55);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(137, 20);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Объект 1 - Точка";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.textBox5);
+            this.panel2.Controls.Add(this.button5);
+            this.panel2.Controls.Add(this.button4);
+            this.panel2.Controls.Add(this.textBox4);
+            this.panel2.Controls.Add(this.textBox3);
+            this.panel2.Controls.Add(this.button3);
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Controls.Add(this.textBox2);
+            this.panel2.Controls.Add(this.textBox1);
+            this.panel2.Location = new System.Drawing.Point(398, 26);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(258, 453);
+            this.panel2.TabIndex = 1;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(59, 406);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(128, 22);
+            this.textBox5.TabIndex = 11;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(28, 358);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(181, 41);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "Диагональ";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(28, 269);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(181, 42);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Длина дуги";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(59, 317);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(128, 22);
+            this.textBox4.TabIndex = 8;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(59, 230);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(128, 22);
+            this.textBox3.TabIndex = 7;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(28, 185);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(181, 39);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Диаметр";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(28, 96);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(181, 43);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Площадь (Виртуальный метод)";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(28, 9);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(181, 41);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Площадь";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(59, 145);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(128, 22);
+            this.textBox2.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(59, 56);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(128, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(707, 621);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ lab5/lab5/Form1.cs
@@ -0,0 +1,280 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
+
+namespace lab5
+{
+    public partial class Form1 : Form
+    {
+        tochka Ob1 = new tochka();
+        okr Ob2 = new okr(10);
+        sektor Ob3 = new sektor(10, 100);
+        rectangle Ob4 = new rectangle(5, 5);
+        tochka t;
+        public Form1()
+        {
+            InitializeComponent();
+            t = Ob2;
+        }
+        
+        // очистка полей после выбора объекта
+        private void Ochistka()
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+            textBox5.Text = "";
+
+        }
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            Ochistka();
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            Ochistka();
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            Ochistka();
+        }
+
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+            Ochistka();
+        }
+        // -- конец очистки
+
+
+        
+        private Boolean radioButtonSelected() // проверка выбранной кнопки
+        {
+
+            if (radioButton1.Checked)
+            {
+                t = Ob1;
+            }
+            else if (radioButton2.Checked)
+            {
+                t = Ob2;
+            }
+            else if (radioButton3.Checked)
+            {
+                t = Ob3;
+            }
+            else if (radioButton4.Checked)
+            {
+                t = Ob4;
+            }
+            else
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private void button1_Click(object sender, EventArgs e) // ПЛОЩАДЬ
+        {
+            if (radioButtonSelected())
+            {
+
+                if (radioButton1.Checked)
+                {
+                    textBox1.Text = Math.Round(Ob1.S(), 2).ToString();
+                }
+                else if (radioButton2.Checked)
+                {
+                    textBox1.Text = Math.Round(Ob2.S(), 2).ToString();
+                }
+                else if (radioButton3.Checked)
+                {
+                    textBox1.Text = Math.Round(Ob3.S(), 2).ToString();
+                }
+                else if (radioButton4.Checked)
+                {
+                    textBox1.Text = Math.Round(Ob4.S(), 2).ToString();
+                }
+
+            }
+            else
+            {
+                MessageBox.Show("Не выбран объект");
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e) // ПЕРЕОПРЕДЕЛЕННЫЙ ВИРТУАЛЬНЫЙ МЕТОД ПЛОЩАДЬ
+        {
+            if (radioButtonSelected())
+            {
+                textBox2.Text = Math.Round(t.SV(), 2).ToString();
+            }
+            else
+            {
+                MessageBox.Show("Не выбран объект");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e) //ДИАМЕТР
+        {
+            if (radioButtonSelected())
+            {
+                if (t is okr)
+                {
+                    textBox3.Text = Math.Round(Ob2.Diametr(), 2).ToString();
+                }
+                else
+                {
+                    MessageBox.Show("Выбранный объект не соответствует методу");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Не выбран объект");
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e) // ДЛИНА ДУГИ
+        {
+            if (radioButtonSelected())
+            {
+
+                if (t is sektor)
+                {
+                    textBox4.Text = Math.Round(Ob3.Ldugi(), 2).ToString();
+                }
+                else if (t is rectangle) {
+                    textBox4.Text = Math.Round(Ob4.Perimetr(), 2).ToString();
+                }
+                else
+                {
+                    MessageBox.Show("Выбранный объект не соответствует методу");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Не выбран объект");
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e) // ДИАГОНАЛЬ
+        {
+            if (radioButtonSelected())
+            {
+                if (t is rectangle)
+                {
+                    textBox5.Text = Math.Round(Ob4.Diag(), 2).ToString();
+                }
+                else
+                {
+                    MessageBox.Show("Выбранный объект не соответствует методу");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Не выбран объект");
+            }
+        }
+
+
+    }
+
+    class tochka // КЛАСС ТОЧКА
+    {
+        public double S()
+        {
+            return 0;
+        }   
+        public virtual double SV()
+        {
+            return 0;
+        }
+    }
+    class okr : tochka
+    {
+        private double R;
+        public okr(double r)
+        {
+            R = r;
+        }
+
+        public new double S()
+        {
+            return Math.PI * R * R;
+        }
+        public override double SV()
+        {
+            return Math.PI * R * R;
+        }
+        public double Diametr()
+        {
+            return R * 2;
+        }
+
+
+    }
+    class sektor : tochka     // СЕКТОР
+    {
+        private double r;
+        private double ugol;
+        public sektor(double r, double ugol)
+        {
+            this.r = r;
+            this.ugol = ugol;
+        }
+        public double S()
+        {
+            return Math.PI * r * r * (ugol / 360);
+        }
+        public override double SV()
+        {
+            return Math.PI * r * r * (ugol / 360);
+        }
+        public double Ldugi() //длина дуги
+        {
+            return Math.PI * r * (ugol / 360);
+        }
+
+    }
+    class rectangle : tochka // прямоугольник
+    {
+        private double a;
+        private double b;
+        public rectangle(double a, double b)
+        {
+            this.a=a;
+            this.b=b;
+        }
+        public double S()
+        {
+            return a * b;
+        }
+        public override double SV()
+        {
+            return a * b;
+        }
+        public double Diag()
+        {
+            return Math.Sqrt(a * a + b * b);
+        }
+
+        public double Perimetr()
+        {
+            return (a + b) * 2;
+        }
+
+    }
+    
+   
+}
