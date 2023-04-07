49f5ee7329bc848cf2b83d83fb010acb1d9c29bc
vikovaro <98817333+vikovaro@users.noreply.github.com>
2023-04-07 16:50:24 +0300

    Add files via upload

---
+++ lab7/lab7/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿using System.Drawing;
+
+namespace lab7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.Location = new System.Drawing.Point(250, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(215, 29);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Удалить элемент";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button2.Location = new System.Drawing.Point(12, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(215, 30);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Вставить элемент";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 174);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(204, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "ЗАПОЛНЕННОСТЬ ОЧЕРЕДИ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Очередь";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ lab7/lab7/Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Contracts;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ProgressBar;
+
+namespace lab7
+{
+    public partial class Form1 : Form
+    {
+
+        // ЕСЛИ ПИЗДИТЕ МОЙ КОД, СНАЧАЛА ДОБАВЬТЕ НА ФОРМУ 2 КНОПКИ И LABLE, А УЖЕ ПОТОМ ВСТАВЛЯЙТЕ КОД
+        
+        Queue myQueue = new Queue(); // 12 - размер накопителя для очереди
+        Random rnd = new Random(); // для генератора рандомных чисел
+
+        public Form1()
+        {
+            InitializeComponent();
+            label2.Text = "Элементов в очереди: " + myQueue.Size().ToString();
+        }
+        
+
+
+
+        // Заполнение очереди рандомными значениям
+        // Невероятно гениальный генератор для заполнения очереди значениями, используется в классе button2_Click
+        // Генерация длины от 1 до 2, и заполнение случайными символами посредством конвертации из ASCII
+        public string Generator()
+        {
+            int nameLength = rnd.Next(1, 3); // генерируем случайное число от 1 до 2
+            string name = "";
+
+            for (int i = 0; i < nameLength; i++)
+            {
+                int asciiValue = rnd.Next(97, 123); // генерация случайного числа от 97 до 122
+                char character = (char)asciiValue; // конвертируем число в ASCII-символ
+                name += character.ToString(); // добавляем символ к имени
+            }
+            return name;
+        }
+
+
+
+        private void button2_Click(object sender, EventArgs e) //добавление
+        {
+            Graphics g = CreateGraphics();
+            myQueue.Enqueue(new unit(Generator().ToString()));
+            myQueue.Draw(g);
+            label2.Text = "Элементов в очереди: " + myQueue.Size().ToString();
+
+        }
+        private void button1_Click(object sender, EventArgs e) //удаление
+        {
+            Graphics g = CreateGraphics();
+
+            if (myQueue.Size() == 0)
+            {
+                MessageBox.Show("Ошибка. Очередь пуста.");
+                return;
+            }
+
+
+            myQueue.Dequeue();
+            myQueue.Draw(g);
+            label2.Text = "Элементов в очереди: " + myQueue.Size().ToString();
+        }
+
+
+        
+
+        //Вариант 4. Класс, содержащий следующие члены.
+        //a.Поле, в котором хранится имя элемента, имя запрашивается у пользователя в момент создания элемента;
+        //b.Метод возвращающий «имя» элемента.
+        //c.Конструктор класса с одним параметром
+
+        //Вариант 6
+        //Очередь.Первый и последний элементы должны быть закрашены как текущие.
+        //На форме должны быть кнопки, выполняющие следующие действия:
+        //a.Удалять элемент.
+        //b.Вставлять элемент.
+
+    }
+    public class Queue
+    {
+        private Queue<unit> queue;
+
+        public Queue()
+        {
+            queue = new Queue<unit>();
+        }
+
+        public void Enqueue(unit item) 
+        {
+            
+            queue.Enqueue(item);
+        }
+
+        public unit Dequeue()
+        {
+            return queue.Dequeue();
+        }
+
+        public bool IsEmpty()
+        {
+            return queue.Count == 0;
+        }
+
+        public int Size()
+        {
+            return queue.Count;
+        }
+
+        
+
+        public unit Peek()
+        {
+            return queue.Peek();
+        }
+
+        public void Draw(Graphics g) //Рисование элементов списка
+        {
+            if (queue.Count != 0)
+            {
+                g.Clear(Color.White);
+
+                g.FillEllipse(new SolidBrush(Color.LightBlue), 21, 50, 20, 20);
+                g.FillEllipse(new SolidBrush(Color.LightBlue), queue.Count * 21, 50, 20, 20);
+                Pen pen = new Pen(Color.Red);
+
+                unit[] items = queue.ToArray();
+                for (int i = 0; i < queue.Count; i++)
+                {
+                    //MessageBox.Show(i.ToString());
+
+                    g.DrawString(items[i].getName(), new Font("Arial", 8), new
+                    SolidBrush(Color.Black), (i+1) * 21 + 3, 52);
+                    g.DrawEllipse(pen, (i+1)* 21, 50, 20, 20);
+
+
+                }
+            }
+            else
+            {
+                g.Clear(Color.White);
+            }
+        }
+
+    }
+    
+    public class unit
+    {
+        private string name;
+
+        public unit(string name)
+        {
+            this.name = name;
+        }
+
+        public string getName()
+        {
+            return name;
+        }
+    }
+}
