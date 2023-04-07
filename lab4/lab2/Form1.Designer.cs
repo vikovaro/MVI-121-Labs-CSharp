2a09e746a85df26830f3b30b236e69a89eda5eee
vikovaro <98817333+vikovaro@users.noreply.github.com>
2023-04-07 16:48:49 +0300

    Add files via upload

---
+++ lab4/lab2/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace lab2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.fToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.LightCoral;
+            this.button1.Location = new System.Drawing.Point(542, 47);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(129, 38);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Вывод";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(542, 282);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(122, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(542, 129);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(162, 22);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.Tag = "";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(542, 91);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(162, 22);
+            this.textBox3.TabIndex = 3;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(84, 28);
+            // 
+            // fToolStripMenuItem
+            // 
+            this.fToolStripMenuItem.Name = "fToolStripMenuItem";
+            this.fToolStripMenuItem.Size = new System.Drawing.Size(83, 24);
+            this.fToolStripMenuItem.Text = "f";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button2.Location = new System.Drawing.Point(542, 310);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(122, 26);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Ввод";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.LightCoral;
+            this.button3.Location = new System.Drawing.Point(542, 483);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(157, 48);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Очистка ListBox";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button4.Location = new System.Drawing.Point(542, 428);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(157, 49);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Вывести информацию ListBox";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.Color.Silver;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(16, 31);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(487, 500);
+            this.listBox1.TabIndex = 7;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(923, 577);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fToolStripMenuItem;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ lab4/lab2/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab2
+{
+    public partial class Form1 : Form
+    {
+        okr[] Mas = new okr[22];
+        public Form1()
+        {
+            InitializeComponent();
+            for (int i = 0; i < 22; i++)
+            {
+                Mas[i] = new okr(i+1);
+            }
+        }
+
+        //ввод
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "") { MessageBox.Show("Не введено число"); return; }
+
+            if (listBox1.SelectedIndex == -1)
+            {
+                MessageBox.Show("Не выделена ячейка"); return;
+            }
+            double r = (double)int.Parse(textBox1.Text);
+            Mas[listBox1.SelectedIndex].setR(r);
+        }
+          //вывод
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int N = listBox1.SelectedIndex;
+            if (N == -1)
+            {
+                MessageBox.Show("Не выделена ячейка"); return;
+            }
+            textBox2.Text = Mas[N].DlinaOkr().ToString();
+            textBox3.Text = Mas[N].Sokr().ToString();
+            textBox1.Text = Mas[N].getR().ToString();
+
+        }
+        //очистить
+        private void button3_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+        }
+        //вывести информацию
+        private void button4_Click(object sender, EventArgs e)
+        {
+            String s; // для формирования строки вывода
+            for (int i = 0; i < 22; i++)
+            {
+                s = "Элемент " + i.ToString() + "; значение R=" + Mas[i].getR().ToString() + "; Длина=" + Math.Round(Mas[i].DlinaOkr(), 2).ToString() + "; Площадь=" + Math.Round(Mas[i].Sokr(), 2).ToString();
+                listBox1.Items.Add(s);
+            }
+        }
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int n = listBox1.SelectedIndex;
+        }
+    }
+    class okr
+    {
+        private double R; 
+        
+        public okr() {
+            Random rnd = new Random();
+            int value = rnd.Next(5, 15);
+            this.R = value; // значение по умолчанию
+        }
+        public okr(double r)
+        {
+            this.R = r;
+        }
+        public double DlinaOkr()
+        {
+            return 2*Math.PI * this.R;
+        }
+        public double Sokr()
+        {
+            return Math.PI * this.R * this.R;
+        }
+        public bool setR(double r)
+        {
+            if (r>4 && r<16) { 
+                this.R = r; 
+                return true;
+            } 
+            
+            MessageBox.Show("Ошибка");
+            return false;
+        }
+
+        public double getR()
+        {
+            return this.R;
+        }
+    }
+}
