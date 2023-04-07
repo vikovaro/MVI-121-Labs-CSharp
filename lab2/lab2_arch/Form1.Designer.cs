0bb668a9f9bcb4aee4ec4aa050f25f69ea6a4eea
vikovaro <98817333+vikovaro@users.noreply.github.com>
2023-04-07 16:46:04 +0300

    Add files via upload

---
+++ lab2/lab2_arch/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace lab2_arch
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(59, 38);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(347, 129);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(157, 22);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(347, 171);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(157, 22);
+            this.textBox3.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.button1.Location = new System.Drawing.Point(184, 37);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(157, 59);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "ввод";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.LightPink;
+            this.button2.Location = new System.Drawing.Point(362, 38);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(142, 58);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "вывод";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.PaleGreen;
+            this.button3.Location = new System.Drawing.Point(59, 129);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(227, 96);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "расчет";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ lab2/lab2_arch/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab2_arch
+{
+    public partial class Form1 : Form
+    {
+        okr o1 = new okr(10); // поле главной формы
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        // кнопка ввод
+        private void button1_Click(object sender, EventArgs e)
+        {
+            o1.R = (double)int.Parse(textBox1.Text);
+        }
+
+        // кнопка вывод
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = o1.DlinaOkr().ToString();
+            textBox3.Text = o1.Sokr().ToString();
+        }
+
+        // кнопка расчет
+        private void button3_Click(object sender, EventArgs e)
+        {
+            double r = (double)int.Parse(textBox1.Text);
+            okr b = new okr(r);
+
+            button3.Text = "Длина окружности:\n" + b.DlinaOkr().ToString() + "\nПлощадь окружности:\n" + b.Sokr().ToString();
+
+
+        }
+
+        class okr
+        {
+            public double R;
+            public okr(double r)
+            {
+                this.R = r;
+            }
+            public double DlinaOkr()
+            {
+                return 2 * Math.PI * this.R;
+            }
+            public double Sokr()
+            {
+                return Math.PI * this.R * this.R;
+            }
+            public void setR(double r)
+            {
+                this.R = r;
+            }
+
+            public double getR()
+            {
+                return this.R;
+            }
+        }
+
+        
+    }
+}
